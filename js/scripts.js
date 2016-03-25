4881210f01a6dc3540b5efbba137d72b0f38be04
bb <blakebless7@gmail.com>
2016-03-25 14:45:14 -0700

    added another form that asks for users name and inputs in into the bottom when the vacation is decided. like 'Blake you're going to disneyland'.

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,38 @@ var travel = $("input:radio[name=travelRadio]:checked").val();
 var appealing = $("input:radio[name=appealingRadio]:checked").val();
 var fun = $("input:radio[name=funRadio]:checked").val();
 var vacation = $("input:radio[name=vacationRadio]:checked").val();
+var insertname = $("input#name").val();
 
 console.log("step two")
 
 if (weather === "one" && travel === "two" && appealing === "three" && fun === "three" && vacation === "one"){
 $(".showme").removeClass("showme");
 $(".Hawaii").addClass("showme");
+$(".insname").text(insertname);
 }
 else if (weather === "three" && travel === "two" && appealing === "three" && fun === "two" && vacation === "three"){
 $(".showme").removeClass("showme");
 $(".Disney").addClass("showme");
+$(".insname").text(insertname);
 }
 else if (weather === "three" && travel === "one" && appealing === "two" && fun === "one" && vacation === "one"){
 $(".showme").removeClass("showme");
 $(".Grand").addClass("showme");
+$(".insname").text(insertname);
 }
 else if (weather === "two" && travel === "two" && appealing === "three" && fun === "one" && vacation === "two"){
 $(".showme").removeClass("showme");
 $(".Italy").addClass("showme");
+$(".insname").text(insertname);
 }
 else if (weather === "one" && travel === "three" && appealing === "one" && fun === "two" && vacation === "two"){
 $(".showme").removeClass("showme");
 $(".Gamboa").addClass("showme");
+$(".insname").text(insertname);
 }else {
 $(".showme").removeClass("showme");
 $(".Disney").addClass("showme");
+$(".insname").text(insertname);
 }
 
 
